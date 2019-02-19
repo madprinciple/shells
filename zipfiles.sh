5696b1c900470962274cf2ab7598c767eb846cd5
madprinciple <antonsriyantha@gmail.com>
2019-02-19 23:59:38 +0530

    modified to keep 3 files minimum, previous bug fixed

--- zipfiles.sh
+++ zipfiles.sh
@@ -4,7 +4,7 @@ cd /home/anton/testDir
 file_count=$(ls -ltr /home/anton/testDir | awk '{print $9}' | sed -n '/file/p' | grep -v 'gz' | wc -l) #geting non zipped file count 
 
 if [ $file_count -ge 5 ] ; then  #starting to zip  if non zipped file count is greater than 5
-    ls -ltr /home/anton/testDir | awk '{print $9}' | sed -n '/file/p' | grep -v 'gz' | head -2 | xargs gzip -fv # always keeping 3 files non zipped
+    ls -ltr /home/anton/testDir | awk '{print $9}' | sed -n '/file/p' | grep -v 'gz' | head -$(($file_count -3)) | xargs gzip -fv # always keeping 3 files non zipped
 else
      echo "file count is not greater than 5"
 fi
